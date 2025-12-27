bf186c32a88b744948bcde44003d5e9f89b0f702
Roberto Maurizio <74676866+robertomaurizio@users.noreply.github.com>
2025-12-27 15:26:45 -0800

    Refactor run_eval.py for improved clarity and structure

--- run_eval.py
+++ run_eval.py
@@ -1,6 +1,4 @@
 # run_eval.py - runner script to visualize results
-
-# run_train.py
 from src.data import *
 from src.model import *
 from src.train import *
@@ -10,11 +8,12 @@ def main():
   df, df_excluded = load_sobol_solps_samples()
 
   # Preprocess data: normalize, rescale, log-transform
-  x_train_scaled, x_val_scaled, x_test_scaled, y_train_scaled, y_val_scaled, y_test_scaled, x_temp, y_temp, scaler_y, x_train, y_train, x_test, y_test = \
-    preprocess_data(df, df_excluded)
+  x_train_scaled, x_val_scaled, x_test_scaled, y_train_scaled, y_val_scaled, y_test_scaled, \
+      x_train, x_val, x_test, y_train, y_val, y_test, \
+      x_temp, y_temp, scaler_y = preprocess_data(df)
 
   # Load TPE results
-  trials = load_TPE_results()
+  best_hyperparameters, trials = load_TPE_results()
 
   # Visualize TPE results
   visualize_tpe_results(trials)
@@ -23,16 +22,16 @@ def main():
   final_model_list, final_history_list = load_model_history_list()
 
   # Visualize multiple seeds results (histograms)
-  mean_mse, std_mse, mean_r2, std_r2, mse_list, r2_list = evaluate_multiple_seeds(final_model_list)
+  mse_list, r2_list = evaluate_multiple_seeds(final_model_list, x_test_scaled, y_test, scaler_y)
 
   # Visualize detailed results for single (representative) network
-  mse_test_nn, r2_test_nn, y_predicted = plot_single_network_predictions(mse_list, r2_list, final_model_list, final_history_list, x_test_scaled, y_test, scaler_y)
+  plot_single_network_predictions(mse_list, r2_list, final_model_list, final_history_list, x_test_scaled, y_test, scaler_y)
 
   # Run linear regression fit
-  y_predicted_test_linear = fit_linear_regression_model(x_train, y_train, x_test, y_test)
+  y_predicted_test_linear, mse_test_linear, r2_test_linear = fit_linear_model(x_train, y_train, x_test, y_test)
 
   # Visualize linear regression fit results
-  plot_linear_regression_predictions(y_test, y_predicted_test_linear)
+  plot_linear_model_predictions(y_test, y_predicted_test_linear, r2_test_linear, mse_test_linear)
 
 if __name__ == "__main__":
     main()
