92af9333e86700bde28ee343343621ee35643182
Roberto Maurizio <74676866+robertomaurizio@users.noreply.github.com>
2025-12-27 15:23:20 -0800

    Refactor data processing and visualization functions

--- src/data.py
+++ src/data.py
@@ -12,7 +12,6 @@ import os
 from datetime import datetime
 
 def generate_sobol_samples():
-  # Generate Sobol sequence of varied input parameters
 
   # Define bounds
   x1_min, x1_max = 2, 20          # Power injected into the tokamak
@@ -44,7 +43,7 @@ def generate_sobol_samples():
   return x1, x2, x3, x4
 
 def load_sobol_solps_samples():
-  
+
   # Mount disk with data
   drive.mount('/content/drive')
 
@@ -75,16 +74,10 @@ def load_sobol_solps_samples():
   df = df_all[(df_all.convMetric < 0.05) & (df_all.runTime > 0.95) & (df_all.tetpeak < df_all.teompsep)]
   df_excluded = df_all.loc[df_all.index.difference(df.index)]
 
-  # Print the minimum and maximum values for each column in the df DataFrame
-  print("Minimum and Maximum values for each column in df:")
-  for col in df.columns:
-      min_val = df[col].min()
-      max_val = df[col].max()
-      print(f"{col}: [Min: {min_val:.4f}, Max: {max_val:.4f}]")
-
   return df, df_excluded
 
 def plot_sobol_solps_samples(df, df_excluded):
+
   # Plot
   plt.figure(figsize=(4, 4))
   plt.scatter(df.power, df.flux/1e2, s=10, color='blue', alpha=0.3, label = 'Converged runs')
@@ -94,7 +87,7 @@ def plot_sobol_solps_samples(df, df_excluded):
   plt.grid(True)
   plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
   plt.show()
-
+    
   # Some preliminary plots - global sanity checks
   fig, axs = plt.subplots(1, 2, figsize=(8, 4))
   axs = axs.flatten()
@@ -121,7 +114,7 @@ def plot_sobol_solps_samples(df, df_excluded):
   plt.show()
 
 def preprocess_data(df):
-  # Preprocess data: standardize features, scale targets
+  # Prepare data for NN training
   x = df[['power', 'flux', 'D', 'Chi']]                           # x shape: [n_data, n_features=4]
   y = df[['necore', 'tecore', 'neompsep', 'teompsep', 'tetpeak']] # y shape: [n_data, n_targets=5]
 
@@ -141,8 +134,6 @@ def preprocess_data(df):
       max_val = x[col].max()
       print(f"{col}: [{min_val:.2f}, {max_val:.2f}]")
   
-  from sklearn.preprocessing import StandardScaler
-
   # Standardize the features
   # NB: normalization parameters are computed only from the training set
   scaler = StandardScaler()
@@ -155,6 +146,25 @@ def preprocess_data(df):
   print(f"Shape of x_val_scaled: {x_val_scaled.shape}")
   print(f"Shape of x_test_scaled: {x_test_scaled.shape}")
 
+  # Concatenate scaled features from train, val, and test sets
+  x_combined_scaled = np.vstack((x_train_scaled, x_val_scaled, x_test_scaled))
+
+  # Define feature names
+  feature_names = x.columns.tolist()
+
+  fig, axs = plt.subplots(2, 2, figsize=(5, 4)) # 4 features, so 2x2 grid
+  axs = axs.flatten()
+
+  for i, feature_name in enumerate(feature_names):
+      axs[i].hist(x_combined_scaled[:, i], bins=30, alpha=0.7, color='lightcoral')
+      axs[i].set_title(f'Distribution of Standardized {feature_name}')
+      axs[i].set_xlabel(f'Standardized {feature_name}')
+      axs[i].set_ylabel('Frequency')
+      axs[i].grid(True)
+
+  plt.tight_layout()
+  plt.show()
+
   # Log-transform the temperature targets (columns = [1,3,4])
   y_train_log, y_val_log, y_test_log = y_train.copy(), y_val.copy(), y_test.copy()
   log_transform_cols = [1, 3, 4]
@@ -163,21 +173,43 @@ def preprocess_data(df):
       y_val_log.iloc[:, col_index] = np.log(y_val.iloc[:, col_index])
       y_test_log.iloc[:, col_index] = np.log(y_test.iloc[:, col_index])
 
-  # Scale the output targets using Min-Max normalization
-  scaler_y = MinMaxScaler()
+  # Standardize the output targets using StandardScaler
+  scaler_y = StandardScaler()
   y_train_scaled = scaler_y.fit_transform(y_train_log)
   y_val_scaled = scaler_y.transform(y_val_log)
   y_test_scaled = scaler_y.transform(y_test_log)
+
+  # Concatenate scaled targets from train, val, and test sets
+  y_combined_scaled = np.vstack((y_train_scaled, y_val_scaled, y_test_scaled))
 
   print("Targets scaled")
   print(f"Shape of y_train_scaled: {y_train_scaled.shape}")
   print(f"Shape of y_val_scaled: {y_val_scaled.shape}")
   print(f"Shape of y_test_scaled: {y_test_scaled.shape}")
 
-  return x_train_scaled, x_val_scaled, x_test_scaled, y_train_scaled, y_val_scaled, y_test_scaled, x_temp, y_temp, scaler_y, x_train, y_train, x_test, y_test 
+  # Define target names
+  target_names = y.columns.tolist()
+
+  fig, axs = plt.subplots(2, 3, figsize=(10, 6))
+  axs = axs.flatten()
+
+  for i, target_name in enumerate(target_names):
+      axs[i].hist(y_combined_scaled[:, i], bins=30, alpha=0.7, color='lightgreen')
+      axs[i].set_title(f'Distribution of Standardized {target_name}')
+      axs[i].set_xlabel(f'Standardized {target_name}')
+      axs[i].set_ylabel('Frequency')
+      axs[i].grid(True)
+
+  plt.tight_layout()
+  plt.show()
+
+  return x_train_scaled, x_val_scaled, x_test_scaled, y_train_scaled, y_val_scaled, y_test_scaled, \
+    x_train, x_val, x_test, y_train, y_val, y_test, \
+    x_temp, y_temp, scaler_y
 
 def save_TPE_results(best_hyperparameters, trials):
   # Save best_hyperparameters and trials to file
+
   # Get today's date
   today_date = datetime.now().strftime("%Y-%m-%d")
 
@@ -195,18 +227,21 @@ def load_TPE_results():
   # - DO NOT RUN IF JUST DID TPE OPTIMIZATION -
   # It will overwrite best_hyperparameters and trials
   # Load best_hyperparameters and trials from files
+
   load_directory = '/content/drive/My Drive/Colab Notebooks'
-  best_hyperparameters_path = os.path.join(load_directory, 'TPE_2025-11-10.pkl')
+  best_hyperparameters_path = os.path.join(load_directory, 'TPE_2025-12-27.pkl')
 
   with open(best_hyperparameters_path, 'rb') as f:
       data = pickle.load(f)
 
   best_hyperparameters = data['best_hyperparameters']
   trials = data['trials']
+
   return best_hyperparameters, trials
 
 def save_model_history_list(final_model_list, final_history_list):
   # Save LIST of final model and history to file
+
   # Get today's date
   today_date = datetime.now().strftime("%Y-%m-%d")
 
@@ -223,12 +258,14 @@ def save_model_history_list(final_model_list, final_history_list):
 def load_model_history_list():
   # - OPTIONAL -
   # Load list of NN models and corresponding histories
+
   load_directory = '/content/drive/My Drive/Colab Notebooks'
-  list_save_path = os.path.join(load_directory, 'NN_model_history_list_2025-12-16.keras')
+  list_save_path = os.path.join(load_directory, 'NN_model_history_list_2025-12-27.keras')
 
   with open(list_save_path, 'rb') as f:
       data = pickle.load(f)
 
   final_model_list = data['final_model_list']
   final_history_list = data['final_history_list']
+  N_models = len(final_model_list)
   return final_model_list, final_history_list
