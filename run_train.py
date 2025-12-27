61e09e850c0cf9261d1b3f005601f85d1bad037c
Roberto Maurizio <74676866+robertomaurizio@users.noreply.github.com>
2025-12-27 15:26:05 -0800

    Refactor data preprocessing and training calls

--- run_train.py
+++ run_train.py
@@ -8,8 +8,9 @@ def main():
   df, df_excluded = load_sobol_solps_samples()
 
   # Preprocess data: normalize, rescale, log-transform
-  x_train_scaled, x_val_scaled, x_test_scaled, y_train_scaled, y_val_scaled, y_test_scaled, x_temp, y_temp, scaler_y, x_train, y_train, x_test, y_test = \
-    preprocess_data(df, df_excluded)
+  x_train_scaled, x_val_scaled, x_test_scaled, y_train_scaled, y_val_scaled, y_test_scaled, \
+      x_train, x_val, x_test, y_train, y_val, y_test, \
+      x_temp, y_temp, scaler_y = preprocess_data(df)
 
   # Define hyperparameter search space for TPE optimization
   num_layers_choices, units_choices, activation_choices, \
@@ -23,10 +24,10 @@ def main():
 
   # Train neural network using multiple random seeds
   final_model_list, final_history_list = (
-    train_multiple_seeds(best_hyperparameters, num_layers_choices, units_choices, activation_choices, learning_rate_choices, batch_size_choices, 
+    train_multiple_seeds(best_hyperparameters, num_layers_choices, units_choices, activation_choices, learning_rate_choices, batch_size_choices,
                          use_batch_norm_choices, x_train_scaled, y_train_scaled, x_val_scaled, y_val_scaled, N_models = 100)
     )
-  
+
   # Save neutral network (multiple seeds) results
   save_model_history_list(final_model_list, final_history_list)
 
